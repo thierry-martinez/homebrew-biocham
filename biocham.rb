39fe94404d497523e7122e601e9c264930b76c91
Thierry Martinez <martinez@nsup.org>
2016-03-14 17:39:02 +0100

    New release

--- biocham.rb
+++ biocham.rb
@@ -1,1 +1,17 @@
+class Biocham < Formula
+  desc ""
+  homepage "https://lifeware.inria.fr/4.0/"
+  url "https://lifeware.inria.fr/biocham/4.0/biocham--src.tar.gz"
+  version "4.0.20160314"
+  sha256 "e831393d3bf75844ea463d71d697334591d8f004ca8036c53dcb9377aa48386c"
 
+  depends_on "swi-prolog", "gsl", "libsbml", "gnuplot", "graphviz"
+
+  def install
+    system "make"
+  end
+
+  test do
+    system "false"
+  end
+end
